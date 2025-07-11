24b4978300d03c21f3087f53cb16291d03af5a02
Simon W <simongwunderlich@gmail.com>
2025-07-11 16:12:37 +1000

    Day 2 part 2

--- 2/Main.java
+++ 2/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
         File input = new File("input.txt");
@@ -10,24 +12,32 @@ public class Main {
         int safeReports = 0;
 
         while(scnr.hasNext()) {
-            Scanner lineScnr = new Scanner(scnr.nextLine());
-            safeReports += analyseReport(lineScnr);
+            String[] lineScnr = scnr.nextLine().split(" ");
+            safeReports += analyseReport(lineScnr, false);
         }
         System.out.println(safeReports);
     }
-    public static int analyseReport(Scanner lineScnr) {
-        int prevInt = lineScnr.nextInt();
+    public static int analyseReport(String[] line, boolean dampening) {
+        int prevInt = Integer.parseInt(line[0]);
         int currentInt = 0;
         double direction = 0;
 
-        while(lineScnr.hasNext()) {
-            currentInt = lineScnr.nextInt();
+        for(int i = 1; i < line.length; i++) {
+            currentInt = Integer.parseInt(line[i]);
 
             if (direction == 0)
                 direction = Math.signum(currentInt - prevInt);
 
             if (Math.abs(currentInt - prevInt) > 3 || currentInt - prevInt == 0 || Math.signum(currentInt - prevInt) != direction) {
-                System.out.printf("%d %d\n", prevInt, currentInt);
+                if (dampening)
+                    return 0;
+                for(int x = 0; x < line.length; x++) {
+                    String[] lineCopy = new String[line.length-1];
+                    System.arraycopy(line, 0, lineCopy, 0, x);
+                    System.arraycopy(line, x + 1, lineCopy, x, line.length - 1 - x);
+                    if (analyseReport(lineCopy, true) == 1)
+                        return 1;
+                }
                 return 0;
             }
             direction = Math.signum(currentInt - prevInt);
