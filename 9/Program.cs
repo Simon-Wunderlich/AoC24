fb072c4521d3d80d384fb5a0f5b51009f0526fa0
Simon W <simongwunderlich@gmail.com>
2025-07-17 21:14:30 +1000

    Day 9 part 2

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
@@ -10,14 +11,14 @@ namespace ConsoleApp3
 		{
 			string textFile = "../../../input.txt";
 			string text = File.ReadAllText(textFile);
-			string[] blocks = convToBlocks(text.ToArray());
-			string[] compressedBlocks = compress(blocks);
+			string[][] blocks = convToBlocks(text.ToArray());
+			string[][] compressedBlocks = compress(blocks);
 			long total = calc(compressedBlocks);
 			Console.WriteLine(total);
 		}
-		static string[] convToBlocks(char[] blocks)
+		static string[][] convToBlocks(char[] blocks)
 		{
-			string[] result = [];
+			string[][] result = [];
 			for (int i = 0; i < blocks.Length; i++)
 			{
 				string blockChar = "";
@@ -25,39 +26,65 @@ namespace ConsoleApp3
 					blockChar = (i / 2).ToString();
 				else
 					blockChar = ".";
+				string[] block = [];
 				for (int j = 0; j < int.Parse(blocks[i].ToString()); j++)
 				{
-					result = result.Append(blockChar.ToString()).ToArray();
+					block = block.Append(blockChar.ToString()).ToArray();
 				}
+				result = result.Append(block).ToArray();
 			}
 			return result;
 		}
 
-		static string[] compress(string[] blocks)
+		static string[][] compress(string[][] blocks)
 		{
-			for (int i = 0; i < blocks.Length; i++)
+			for (int j = blocks.Length - 1; j > 0; j--)
 			{
-				if (blocks[i] != ".")
+				if (blocks[j].Length == 0)
 					continue;
-				for (int j = blocks.Length - 1; j > i; j--)
+				if (string.Join("", blocks[j]).Contains("."))
+					continue;
+				for (int i = 0; i <= j; i++)
 				{
-					if (blocks[j] == ".")
+					if (!string.Join("", blocks[i]).Contains("."))
+						continue;
+					if (blocks[i].Length == 0)
+						continue;
+
+					if (blocks[i].Length < blocks[j].Length)
 						continue;
 					//Swap positions
-					(blocks[i], blocks[j]) = (blocks[j], blocks[i]);
+					string[] freeSpace = [];
+					for (int k = 0; k < blocks[j].Length; k++)
+					{
+						freeSpace = freeSpace.Append(".").ToArray();
+					}
+					List<string[]> blockList = blocks.ToList();
+					blockList.Insert(i, blocks[j]);
+					blocks = blockList.ToArray();
+					(blocks[i + 1], blocks[j + 1]) = (blocks[i + 1][blocks[j + 1].Length..], freeSpace);
 					break;
 				}
 			}
 			return blocks;
 		}
-		static long calc(string[] blocks)
+		static long calc(string[][] blocks)
 		{
 			long total = 0;
-			for (long i = 0; i < blocks.Length; i++)
+			int blockNum = 0;
+			for (int i = 0; i < blocks.Length; i++)
 			{
-				if (blocks[i] == ".")
-					break;
-				total += int.Parse(blocks[i]) * i;
+				if (string.Join("", blocks[i]).Contains("."))
+				{
+					blockNum += string.Join("", blocks[i]).Length;
+					continue;
+				}
+				foreach (string block in blocks[i])
+				{
+					total += int.Parse(block) * blockNum;
+					blockNum++;
+
+				}
 			}
 			return total;
 		}
