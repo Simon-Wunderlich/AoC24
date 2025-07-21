44d8c2e80998abd9db998fda8b5d45bb615cecfd
simon <s4172040@student.rmit.edu.au>
2025-07-21 18:28:12 +1000

    Day 13 part 2

--- 13/13.py
+++ 13/13.py
@@ -1,6 +1,4 @@
 import re
-from functools import lru_cache
-
 
 class buttons():
     xA = 0
@@ -15,44 +13,41 @@ with open("input.txt") as f:
 
 def parseClaw(input):
     buttonArr = []
-    p = re.compile("(\+|=)(.+?)(,|$)")
+    p = re.compile("([+=])(.+?)(,|$)")
     for x in input.split("\n"):
         result = p.findall(x)
         buttonArr.append([result[0][1], result[1][1]])
-    h = buttons()
-    h.xA = int(buttonArr[0][0])
-    h.yA = int(buttonArr[0][1])
-    h.xB = int(buttonArr[1][0])
-    h.yB = int(buttonArr[1][1])
-    h.xT = int(buttonArr[2][0])
-    h.yT = int(buttonArr[2][1])
-    return h
+    btns = buttons()
+    btns.xA = int(buttonArr[0][0])
+    btns.yA = int(buttonArr[0][1])
+    btns.xB = int(buttonArr[1][0])
+    btns.yB = int(buttonArr[1][1])
+    btns.xT = int(buttonArr[2][0]) + 10000000000000
+    btns.yT = int(buttonArr[2][1]) + 10000000000000
+    return btns
 
 currentButtons = buttons()
 
+def calcPrice():
+    xA = currentButtons.xA
+    yA = currentButtons.yA
+    xB = currentButtons.xB
+    yB = currentButtons.yB
+    xT = currentButtons.xT
+    yT = currentButtons.yT
 
-@lru_cache
-def calcPrice(a,b, score):
-    if [currentButtons.xT, currentButtons.yT] == findPos(a,b):
-        return 3*a+b
+    b = (xA*yT - xT*yA) / (yB * xA - xB * yA)
+    a = (xT - b * xB) / xA
 
-    if findPos(a,b)[0] > currentButtons.xT and findPos(a,b)[1] > currentButtons.yT or a+b>200:
-        return float('inf')
-
-    temp1 = calcPrice(a+1, b, score)
-    temp2 = calcPrice(a, b+1, score)
-
-    return min(score, temp1, temp2)
-
-def findPos(a,b):
-    return [currentButtons.xA * a + currentButtons.xB * b, currentButtons.yA * a + currentButtons.yB * b]
+    if  a < 0 and b < 0:
+        return 0
+    if (a+b) % 1 != 0:
+        return 0
+    return 3*a+b
 
 total = 0
 for x in claws:
     currentButtons = parseClaw(x)
-    price = calcPrice(0,0,float('inf'))
-    price = 0 if price == float('inf') else price
+    price = calcPrice()
     total+=price
-    print(price)
-    calcPrice.cache_clear()
 print(total)
