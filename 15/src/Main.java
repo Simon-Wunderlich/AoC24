4f6329b431d10b116a9e6fad96f26fe971dbce8f
simon <s4172040@student.rmit.edu.au>
2025-07-23 10:16:46 +1000

    Day 15 part 2

--- 15/src/Main.java
+++ 15/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,8 +6,7 @@ import java.util.Scanner;
 public class Main {
     static int y = 0;
     static int x = 0;
-
-    static ArrayList<String[]> prevMap = new ArrayList<>();
+    static boolean obstacleEncountered = false;
 
     public static void main(String[] args) throws IOException {
         File input = new File("15/src/input.txt");
@@ -20,20 +18,24 @@ public class Main {
 
         while(scnr.hasNext()) {
             String line = scnr.nextLine();
+            line = line.replaceAll("\\.", "..");
+            line = line.replaceAll("@", "@.");
+            line = line.replaceAll("O", "[]");
+            line = line.replaceAll("#", "##");
             if (line.contains("@")) {
                 y = count;
                 x = line.indexOf("@");
             }
             map.add(line.split(""));
             count++;
-            if (line.length() == 0)
+            if (line.isEmpty())
                 break;
         }
-        String instructions = "";
+        StringBuilder instructions = new StringBuilder();
         while(scnr.hasNextLine()) {
-            instructions += scnr.nextLine();
+            instructions.append(scnr.nextLine());
         }
-        for(String step : instructions.split("")) {
+        for(String step : instructions.toString().split("")) {
             for(String[] line : map) {
                 System.out.println(String.join("", line));
             }
@@ -47,7 +49,7 @@ public class Main {
         int gpsSum = 0;
         for(int y = 0; y < map.size(); y++) {
             for(int x = 0; x < map.get(y).length; x++) {
-                if (map.get(y)[x].equals("O"))
+                if (map.get(y)[x].equals("["))
                     gpsSum+= 100*y + x;
             }
         }
@@ -60,18 +62,36 @@ public class Main {
         int _x = x;
         int _y = y;
         System.out.println("Step: " + step);
-        map = propagate(map, dir, _x, _y);
+
+        ArrayList<String[]> prevMap = new ArrayList<>();
+        for(String[] line : map) {
+            prevMap.add(line.clone());
+        }
+        propagate(map, dir, _x, _y);
+        if (obstacleEncountered) {
+            System.out.println("Obstacle Encountered!");
+            x = _x;
+            y =  _y;
+            obstacleEncountered = false;
+            return prevMap;
+        }
         return map;
     }
 
-    public static ArrayList<String[]> propagate(ArrayList<String[]> map, int[] dir, int bx, int by) {
+    public static void propagate(ArrayList<String[]> map, int[] dir, int bx, int by) {
+        if (obstacleEncountered)
+            return;
         int _x = bx + dir[0];
         int _y = by + dir[1];
-        if  (map.get(_y)[_x].equals("O")) {
+        if  (map.get(_y)[_x].equals("]")) {
             propagate(map, dir, _x, _y);
+            if (dir[0] == 0)
+                propagate(map, dir, _x-1, _y);
         }
         if  (map.get(_y)[_x].equals("[")) {
             propagate(map, dir, _x, _y);
+            if (dir[0] == 0)
+                propagate(map, dir, _x+1, _y);
         }
         if (map.get(_y)[_x].equals(".")) {
             if (map.get(by)[bx].equals("@")) {
@@ -81,22 +101,18 @@ public class Main {
             String temp = map.get(_y)[_x];
             map.get(_y)[_x] = map.get(by)[bx];
             map.get(by)[bx] = temp;
+        } else if ((map.get(_y)[_x].equals("#"))) {
+            obstacleEncountered = true;
         }
-        return map;
     }
 
     public static int[] parseDir(String step) {
-        switch (step) {
-            case ">":
-                return new int[] {1,0};
-            case "<":
-                return new int[] {-1,0};
-            case "^":
-                return new int[] {0,-1};
-            case "v":
-                return new int[] {0,1};
-            default:
-                return new int[] {0,0};
-        }
+        return switch (step) {
+            case ">" -> new int[]{1, 0};
+            case "<" -> new int[]{-1, 0};
+            case "^" -> new int[]{0, -1};
+            case "v" -> new int[]{0, 1};
+            default -> new int[]{0, 0};
+        };
     }
 }
